2577c9950f8115ce0e68ce1960c04bcb5e70f648
Priyanshu Dey <priyanshudey.ds@gmail.com>
2024-07-17 18:41:23 +0530

    setup done

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,20 @@
 # configration test
 # test cases
+# in test_generic (# Assertion function, it should be true if test gets passed( eg :a == b ))
+
+
+import pytest
+
+class NotinRange(Exception):
+    def __init__(self,   message= "Value Not In Range"):
+        self.message=message
+        super().__init__(self.message)
+
+
 
 def test_generic():
-    a=2
-    b=2
-    assert a == b   # Assertion function, it should be true if test gets passed( eg :a == b )
+    a=5
+    with pytest.raises(NotinRange):
+        if a not in range(10,20):
+            raise NotinRange
+    
