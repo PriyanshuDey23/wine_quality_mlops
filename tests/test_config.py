d5aa08fcb23282e6e47645d73605bac97910eaf1
Priyanshu Dey <priyanshudey.ds@gmail.com>
2024-07-27 20:09:54 +0530

    creating test

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,17 +4,77 @@
 
 
 import pytest
+import logging
+import os
+import joblib
+import json
+from prediction_service.prediction import form_response,api_response
+import prediction_service # For generating the exception
 
-class NotinRange(Exception):
-    def __init__(self,   message= "Value Not In Range"):
-        self.message=message
-        super().__init__(self.message)
+# we are going to specify the customized data
+
+# Input Data (Example for each range)
+input_data = {
+    "incorrect_range": 
+    {"fixed_acidity": 7897897, 
+    "volatile_acidity": 555, 
+    "citric_acid": 99, 
+    "residual_sugar": 99, 
+    "chlorides": 12, 
+    "free_sulfur_dioxide": 789, 
+    "total_sulfur_dioxide": 75, 
+    "density": 2, 
+    "pH": 33, 
+    "sulphates": 9, 
+    "alcohol": 9
+    },
+
+    "correct_range":
+    {"fixed_acidity": 5, 
+    "volatile_acidity": 1, 
+    "citric_acid": 0.5, 
+    "residual_sugar": 10, 
+    "chlorides": 0.5, 
+    "free_sulfur_dioxide": 3, 
+    "total_sulfur_dioxide": 75, 
+    "density": 1, 
+    "pH": 3, 
+    "sulphates": 1, 
+    "alcohol": 9
+    },
+
+    "incorrect_col":
+    {"fixed acidity": 5, 
+    "volatile acidity": 1, 
+    "citric acid": 0.5, 
+    "residual sugar": 10, 
+    "chlorides": 0.5, 
+    "free sulfur dioxide": 3, 
+    "total_sulfur dioxide": 75, 
+    "density": 1, 
+    "pH": 3, 
+    "sulphates": 1, 
+    "alcohol": 9
+    }
+}
+
+TARGET_range = {
+    "min": 3.0,
+    "max": 8.0
+}
+
+# We are going to do some test
+
+# Correct Range
+# Webapp
+def test_form_response_correct_range(data=input_data["correct_range"]):   # The prediction we are getting is in the right range or not
+    res=form_response(data)
+    assert TARGET_range["min"] <= res <= TARGET_range["max"]   # falls in the range
+
+# For API
+def test_api_form_response_correct_range(data=input_data["correct_range"]):   # The prediction we are getting is in the right range or not
+    res=api_response(data)
+    assert TARGET_range["min"] <= res["response"] <= TARGET_range["max"]   # falls in the range,res is inform of dictionary
 
 
 
-def test_generic():
-    a=5
-    with pytest.raises(NotinRange):
-        if a not in range(10,20):
-            raise NotinRange
-    
