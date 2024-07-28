eeec9e0cf0ba1e61dc6f8eaf12d10f3a08e8de9a
Priyanshu Dey <priyanshudey.ds@gmail.com>
2024-07-28 15:02:21 +0530

    test done

--- tests/test_config.py
+++ tests/test_config.py
@@ -95,3 +95,6 @@ def test_api_response_incorrect_range(data=input_data["incorrect_range"]):
 def test_api_response_incorrect_col(data=input_data["incorrect_col"]):
     res = api_response(data)
     assert res["response"] == prediction_service.prediction.NotInCols().message # We want to get the message
+
+
+# Run pytest -v
